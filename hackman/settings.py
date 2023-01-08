10fd41743d7548ef8ab49a407ee56f810114437f
adityakotha03 <86183042+adityakotha03@users.noreply.github.com>
2023-01-08 15:02:39 +0530

    Add files via upload

--- hackman/settings.py
+++ hackman/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fphvs^&2uvvp&du89_0-3g0(nl%4ao$3t%j6br&&hfi@f#dzef'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','hackmanthan-sentiment-analysis.herokuapp.com']
 
